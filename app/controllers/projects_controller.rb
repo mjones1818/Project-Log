7c68d9e43c126937bf8072a9460b88d986cf322e
mjones1818 <michael.jones0532@gmail.com>
2020-12-17 12:38:46 -0600

    Before form redo

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,9 @@ class ProjectsController < ApplicationController
   
   def new
     @project = Project.new
-    @project.parts.build
+    @project_parts = @project.project_parts.build
+    @project_parts.build_part
+    @parts = Parts.all
   end
 
   def create
@@ -35,6 +37,8 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:name, :brief_description, :main_description, :public, :type_id, :user_id, images: [], parts_attributes: Part.attribute_names.map(&:to_sym).push(:_destroy))
+    params.require(:project).permit(:name, :brief_description, :main_description, :public, :type_id, :user_id, images: [], parts_attributes: [:id, :name, :description, :link, :created_at, :updated_at, :_destroy, project_part: [:quantity]])
   end
 end
+
+#Part.attribute_names.map(&:to_sym).push(:_destroy)
