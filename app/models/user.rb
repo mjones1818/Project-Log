640e49d5d1a635f47c36078c9c7350342038d641
mjones1818 <michael.jones0532@gmail.com>
2020-12-30 10:40:14 -0600

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :projects
   has_secure_password
   validates :email, uniqueness: true
+  validates :name, uniqueness: true
   validates :name, presence: true
   validates(:password, { :length => { :in => 6..20 } })
 end
