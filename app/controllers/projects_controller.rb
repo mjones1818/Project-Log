24ff185bddabc4e5630575f4f91526d8e0f70028
mjones1818 <michael.jones0532@gmail.com>
2020-12-29 18:12:18 -0600

    added prefilled values to project part edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,6 @@ class ProjectsController < ApplicationController
     @project.save
     @project.part_ids += existing_parts
     if @project.save #this is where validations happen
-      #@project.parts.each
       @project.images.purge
       @project.images.attach(project_params[:images])
       if @project.parts.any?
@@ -45,6 +44,33 @@ class ProjectsController < ApplicationController
       redirect_to project_path(@project)
     end
   end
+
+  def edit
+    @project = Project.find_by(id: params[:id])
+    @parts = Part.where.not(id: @project.part_ids)
+  end
+
+  def update
+    
+    @project = Project.find_by(id: params[:id])
+    byebug
+    @project.update(project_params)
+    @project.part_ids += existing_parts
+    if @project.save #this is where validations happen
+      if !project_params[:images].nil?
+        @project.images.purge
+        @project.images.attach(project_params[:images])
+      end
+      if @project.parts.any?
+        redirect_to "/project/#{@project.id}/parts/quantities" and return
+        render 'parts/new'
+      end
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
   private
   
   def require_login
--- app/helpers/parts_helper.rb
+++ app/helpers/parts_helper.rb
@@ -1,2 +1,6 @@
 module PartsHelper
+  def quantity_field(part)
+    project_part = @project.project_parts.find_by(part_id: part.id)
+    project_part.quantity
+  end
 end
